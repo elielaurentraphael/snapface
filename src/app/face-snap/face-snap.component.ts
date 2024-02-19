ffd6da4e100ccaf302f67735f2a8d80b8a8b28cb
elielaurentraphael <raphelaur@gmail.com>
2024-02-19 12:27:38 +0300

    commit n°13: Un utilisateur ne puisse ajouter qu'un seul snap par image. Si l'utilisateur snapped une image, le texte du bouton devient 'Oops, unSnap!', et cliquer dessus doit 'retirer' le snap (réduire les snaps de 1), auquel moment le bouton réaffiche 'Oh Snap!', et retrouve son comportement d'origine - 1 - Dans face-snap.component.ts: ajoutons et initialisons une nouvelle propriété buttonText et à la place de la méthode onAddSnap, met la méthode onClick - 2 - Dans face-snap.component.html: onAddSnap --> onClick et, Oh Snap! --> {{ buttonText }}

--- src/app/face-snap/face-snap.component.ts
+++ src/app/face-snap/face-snap.component.ts
@@ -11,6 +11,7 @@ export class FaceSnapComponent implements OnInit {
   createdDate!: Date;
   snaps!: number;
   imageUrl!: string;
+  buttonText!: string;
 
   ngOnInit() {
     this.title = 'Archibald';
@@ -19,9 +20,16 @@ export class FaceSnapComponent implements OnInit {
     this.snaps = 6;
     this.imageUrl =
       'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg';
+    this.buttonText = 'Oh Snap!';
   }
 
-  onAddSnap() {
-    this.snaps++;
+  onClick() {
+    if (this.buttonText === 'Oh Snap!') {
+      this.snaps++;
+      this.buttonText = 'Oops, unSnap!';
+    } else {
+      this.snaps--;
+      this.buttonText = 'Oh Snap!';
+    }
   }
 }
