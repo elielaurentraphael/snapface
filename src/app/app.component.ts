2d6521c76f68d96fc673157fb4006478584ad5e7
elielaurentraphael <raphelaur@gmail.com>
2024-02-20 18:40:41 +0300

    commit n°28: Restructuration de l'application - 1 - créons un nouveau component ng generate component face-snap-list (FaceSnapListComponent sera le nouveau parent direct de FaceSnapComponent), donc - 2 - Déplaçons lecontenu de AppComponent dans FaceSnapListComponent (app.component.ts dans face-snap-list.component.ts et, app.component.html dans face-snap-list.component.html - 3 - Dans app.component.html: <app-face-snap-list></app-face-snap-list>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,45 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { FaceSnap } from './models/face-snap.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-
-// on déclrare un tableau faceSnaps
-export class AppComponent implements OnInit {
-  faceSnaps!: FaceSnap[];
-
-  // on initialise le tableau ci-dessus
-  ngOnInit() {
-    this.faceSnaps = [
-      {
-        title: 'Archibald',
-        description: 'Mon meilleur ami depuis tout petit !',
-        imageUrl:
-          'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg',
-        createdDate: new Date(),
-        snaps: 35,
-        location: 'Paris',
-      },
-      {
-        title: 'Three Rock Mountain',
-        description: 'Un endroit magnifique pour les randonnées.',
-        imageUrl:
-          'https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Three_Rock_Mountain_Southern_Tor.jpg/2880px-Three_Rock_Mountain_Southern_Tor.jpg',
-        createdDate: new Date(),
-        snaps: 220,
-        location: 'la montagne',
-      },
-      {
-        title: 'Un bon repas',
-        description: "Mmmh que c'est bon !",
-        imageUrl:
-          'https://wtop.com/wp-content/uploads/2020/06/HEALTHYFRESH.jpg',
-        createdDate: new Date(),
-        snaps: 60,
-      },
-    ];
-  }
-}
+export class AppComponent {}
