0161c407e18898dbcdf3e23fab5f94b294efbfe8
elielaurentraphael <raphelaur@gmail.com>
2024-02-20 21:47:28 +0300

    commit n°31: Peuplement et utilisation du FaceSnapsService - 1 - Déplacer l'array faceSnaps dans FaceSnapsService - 2 - On injecte FaceSnapsService dans FaceSnapListComponent: constructor(private faceSnapsService: FaceSnapsService) {} - 3 - On cherche (recupère) le tableau faceSnaps dans le service FaceSnapsService: ngOnInit(...) { this.faceSnaps = this.faceSnapsService.faceSnaps; }

--- src/app/services/face-snaps.service.ts
+++ src/app/services/face-snaps.service.ts
@@ -1,6 +1,38 @@
 import { Injectable } from '@angular/core';
+import { FaceSnap } from '../models/face-snap.model';
 
 @Injectable({
   providedIn: 'root',
 })
-export class faceSnapsService {}
+
+// On déplace ici le tableau faceSnaps
+export class FaceSnapsService {
+  // Un service n'a pas de méthode  ngOnInit(), car les services ne sont pas instanciés de la même manière que les components. Il faudra donc déclarer et initialiser le tableau dans la même expression
+  faceSnaps: FaceSnap[] = [
+    {
+      title: 'Archibald',
+      description: 'Mon meilleur ami depuis tout petit !',
+      imageUrl:
+        'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg',
+      createdDate: new Date(),
+      snaps: 35,
+      location: 'Paris',
+    },
+    {
+      title: 'Three Rock Mountain',
+      description: 'Un endroit magnifique pour les randonnées.',
+      imageUrl:
+        'https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Three_Rock_Mountain_Southern_Tor.jpg/2880px-Three_Rock_Mountain_Southern_Tor.jpg',
+      createdDate: new Date(),
+      snaps: 220,
+      location: 'la montagne',
+    },
+    {
+      title: 'Un bon repas',
+      description: "Mmmh que c'est bon !",
+      imageUrl: 'https://wtop.com/wp-content/uploads/2020/06/HEALTHYFRESH.jpg',
+      createdDate: new Date(),
+      snaps: 60,
+    },
+  ];
+}
