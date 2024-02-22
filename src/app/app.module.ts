c92593bec6adf1da1541ea3f731b0eb3cfef9dd2
elielaurentraphael <raphelaur@gmail.com>
2024-02-22 15:59:21 +0300

    commit n°39: CREATION D'UN MODULE DE ROUTING - 1 - Dans app, créez app-routing.module.ts: déclarer une classe avec le décorateur  @NgModule - 2 -  initialiser la const routes, de type Routes  (importé depuis  @angular/router) et qui contiendra le tableau des routes de l'application - 3 - Créez la route  facesnaps  qui affichera FaceSnapListComponent - Importer FaceSnapListComponent depuis son dossier - 4  Enregistrer RouterModule.forRoot(routes) dans l'array imports et, RouterModule dans l'array exports - Importer RouterModule depuis @angular/router - 5 - Ajouter AppRoutingModule à l'array imports de AppModule, importer AppRoutingModule depuis son dossier - 6 - Dans app.Component.html, remplacez <app-face-snap-list> par <router-outlet>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,19 @@ import { AppComponent } from './app.component';
 import { FaceSnapComponent } from './face-snap/face-snap.component';
 import { FaceSnapListComponent } from './face-snap-list/face-snap-list.component';
 import { HeaderComponent } from './header/header.component';
+// importation de AppRoutingModule
+import { AppRoutingModule } from './app-routing.module';
 
+// importation de AppRoutingModule pour qu'on puisse l'utiliser dans l'application
 @NgModule({
   declarations: [
     AppComponent,
     FaceSnapComponent,
     FaceSnapListComponent,
-    HeaderComponent
+    HeaderComponent,
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
