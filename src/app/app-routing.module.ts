3f16bfdce53175d5e1b7930b0700fce9b7f1c966
elielaurentraphael <raphelaur@gmail.com>
2024-02-23 08:45:50 +0300

    commit n°40: NAVIGATION - 1 - Créer un component nommé LandingPageComponent Dans le terminal: ng g c landing-page - 2 - L'associer à la route vide du routing Dans app-routing.module.ts: path: '', component: LandingPageComponent - Importer LandingPageComponent depuis son dossier - 3 - Dans landing-page.component.html: structure html de LandingPageComponent - 4 - Dans landing-page.component.scss: un peu de styles (personnalisées + bootstrap v5.3.2) - 5 - Dans header.component.html: affichage de la navigation avec le router d'angular - 6 - Dans header.component.scss: un peu de styles pour la navigation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { FaceSnapListComponent } from './face-snap-list/face-snap-list.component';
+// import de LandingPageComponent depuis son dossier
+import { LandingPageComponent } from './landing-page/landing-page.component';
 
+// création d'une route vide correspondant à LandingPageComponent
 const routes: Routes = [
   { path: 'facesnaps', component: FaceSnapListComponent },
+  { path: '', component: LandingPageComponent },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { FaceSnapListComponent } from './face-snap-list/face-snap-list.component
 import { HeaderComponent } from './header/header.component';
 // importation de AppRoutingModule
 import { AppRoutingModule } from './app-routing.module';
+import { LandingPageComponent } from './landing-page/landing-page.component';
 
 // importation de AppRoutingModule pour qu'on puisse l'utiliser dans l'application
 @NgModule({
@@ -15,6 +16,7 @@ import { AppRoutingModule } from './app-routing.module';
     FaceSnapComponent,
     FaceSnapListComponent,
     HeaderComponent,
+    LandingPageComponent,
   ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
