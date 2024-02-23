8e5c78a0b74ebc83deb5cffff419cf80bf47c8b9
elielaurentraphael <raphelaur@gmail.com>
2024-02-23 11:19:22 +0300

    commit n°41: NAVIGATION PROGRAMMATIQUE - lier une méthode au clic sur un bouton - 1 - Dans landing-page.component.ts: créer la méthode onContinue - 2 - Dans landing-page.component.html: transforme le lien en bouton et y lie la méthode onContinue - 3 - Dans landing-page.component.scss: stylez le button - 4 - Dans landing-page.component.ts: importe Router depuis @angular/router - 5 - Injecter le Router dans LandingPageComponent - 6 - Appeler la méthode navigateByUrl du Router depuis la méthode onContinue.

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+// (primo) importation du Router depuis @angular/router
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-landing-page',
   templateUrl: './landing-page.component.html',
-  styleUrl: './landing-page.component.scss'
+  styleUrl: './landing-page.component.scss',
 })
 export class LandingPageComponent {
+  // (secondo) injection du Router dans LandingPageComponent
+  constructor(private router: Router) {}
 
+  onContinue() {
+    // (tertio) appelle de la méthode navigateByUrl() du Router
+    this.router.navigateByUrl('facesnaps');
+  }
 }
